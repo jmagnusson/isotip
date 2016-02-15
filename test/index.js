d203745264f1abf22b60be4121c0ef4a6d34d110
Jacob Magnusson <m@jacobian.se>
2016-02-15 10:52:23 +0100

    Show that the test fails for data-tooltip-autoclose="false" tooltips as well

--- test/index.js
+++ test/index.js
@@ -437,7 +437,7 @@ describe('tooltip triggers', function () {
       expect(document.documentElement.contains(tooltipTmp)).to.be.ok
 
       done()
-    }, isotip.options.removalDelay + 1)
+    }, isotip.options.removalDelay * 10)
   })
 
   it('should not close a tooltip if autoClose is set to false programmatically', function (done) {
